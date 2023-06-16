5f574c27f7e8fcc03cf1e88f3891f1d24a53078e
Tunahan Damar <tunahandamar14@gmail.com>
2023-06-16 22:05:40 +0300

    16062023

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,4 +10,12 @@ public class Login {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(xpath = "//*[@class='header_link ms-2']")
+    public WebElement homePageLoginButton;
+    @FindBy(xpath = "(//input)[1]")
+    public WebElement userNameTextBox;
+    @FindBy(xpath = "(//input)[2]")
+    public WebElement pswTextBox;
+    @FindBy(xpath = "//*[@class='fw-semibold btn btn-primary']")
+    public WebElement loginButton;
 }
--- src/test/java/pages/US17_US18_TeacherStudentInfo.java
+++ src/test/java/pages/US17_US18_TeacherStudentInfo.java
@@ -1,8 +1,25 @@
 package pages;
 
-import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class US17_US18_TeacherStudentInfo extends Login{
+
+    @FindBy(xpath = "//*[text()='Student Info List']")
+    public WebElement studentInfoListTextTuna;
+    @FindBy(xpath = "//table")
+    public WebElement studentInfoListTableTuna;
+    @FindBy(xpath = "(//td//button)[1]")
+    public WebElement editButtonTuna;
+    @FindBy(xpath = "((//form)[2])//select")
+    public WebElement editStudentInfoTuna;
+    @FindBy(xpath = "(//input[@id='absentee'])[2]")
+    public WebElement editStudentInfoAbsanteeTuna;
+    @FindBy(xpath = "(//td)[7]//span")
+    public WebElement infoNoteTable;
+    @FindBy(xpath = "(//td//button)[2]")
+    public WebElement studentInfoDeleteButtonTuna;
+
+
 
 }
