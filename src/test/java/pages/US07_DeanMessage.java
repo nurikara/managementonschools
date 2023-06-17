cf0eae2ba9013bd262076b4e0858c52583a15831
ismail özer <makikohoca@gmail.com>
2023-06-17 17:48:55 +0300

    1706iö

--- src/test/java/pages/US07_DeanMessage.java
+++ src/test/java/pages/US07_DeanMessage.java
@@ -1,8 +1,38 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US07_DeanMessage extends Login {
+
+    @FindBy(xpath = "//button[text()='Menu']")
+    public WebElement MenuButton;
+
+    @FindBy(xpath = "//div[3]/div[2]/div/a[2]")
+    public WebElement ContactGetAll ;
+
+  @FindBy(xpath = "//h5")
+    public WebElement ContactMessagePage ;
+  @FindBy(xpath = "//table/thead/tr/th[1]")
+    public WebElement NameColumn ;
+  @FindBy(xpath = "//table/thead/tr/th[2]")
+    public WebElement EmailColumn ;
+  @FindBy(xpath = "//table/thead/tr/th[3]")
+    public WebElement DateColumn ;
+  @FindBy(xpath = "//table/thead/tr/th[4]")
+    public WebElement SubjectColumn ;
+  @FindBy(xpath = "//table/thead/tr/th[5]")
+    public WebElement MessageColumn ;
+
+ @FindBy(xpath = "//div[3]/div[2]/div/a[3]")
+    public WebElement LogoutButton ;
+
+ @FindBy(xpath = "//button[@class='btn btn-warning']")
+    public WebElement YesButton ;
+
+
+
 
 }
--- src/test/java/pages/US19_US20_TeacherAddMeetList.java
+++ src/test/java/pages/US19_US20_TeacherAddMeetList.java
@@ -1,8 +1,55 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US19_US20_TeacherAddMeetList extends Login {
+
+
+// Teacher ile giris sonrası sagdaki menu tusu
+    @FindBy(xpath = "//button[text()='Menu']")
+    public WebElement MenuButton;
+
+ @FindBy(xpath = "//div[3]/div[2]/div/a[2]")
+    public WebElement MeetManagement;
+
+    @FindBy(xpath = "//*[@id='react-select-3-placeholder']")
+    public WebElement SelectStudentMenuLine ;
+
+    @FindBy(xpath = "//*[@class=' css-9jq23d']")
+    public WebElement SelectedStudent;
+
+@FindBy(xpath = "//input[@id='date']")
+    public WebElement date;
+
+@FindBy(xpath = "//input[@id='startTime']")
+    public WebElement StartTime;
+
+
+@FindBy(xpath = "//input[@id='stopTime']")
+    public WebElement StopTime;
+
+@FindBy(xpath = "//input[@id='description']")
+    public WebElement Description;
+
+@FindBy(xpath = "//button[text()='Submit']")
+    public WebElement addMeetSubmit;
+
+//ileri tarih ile kayıt halinde ve meet kaydı sonucu acılan alert ikisinin locate aynı
+@FindBy(xpath = "//div[@class='Toastify__toast-body']")
+    public WebElement MeetPageAlert;
+
+@FindBy(xpath = "(//div[@class='invalid-feedback'])[2]")
+    public WebElement StartTimeRequired;
+
+@FindBy(xpath = "(//div[@class='invalid-feedback'])[3]")
+    public WebElement StopTimeRequired;
+
+@FindBy(xpath = "(//div[@class='invalid-feedback'])[4]")
+    public WebElement DescriptionRequired;
+
+
 
 }
