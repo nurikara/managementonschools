a39d7629df14bb361721771251f22f35094fa7d2
masutcu <masutcu@gmail.com>
2023-06-20 21:23:43 +0300

    db

--- src/test/java/runners/runnerMali.java
+++ src/test/java/runners/runnerMali.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         monochrome = false,//konsolu okunakli hala getirir false olunca kullanılmayacak.
         features = "src/test/resources/features",
         glue = {"stepdef", "hooks"},//bu parametre ile kodalrımızı yazdıgımız stepDefinition classının package ını belirtiriz.
-        tags = "@step2",//buraya herhangi birsey yazabiliriz. biz bu tagsı nerede kullacagız:benim calıstırmak ıstedıgım senaryoya isim verebilmemdir.
+        tags = "@db" ,//buraya herhangi birsey yazabiliriz. biz bu tagsı nerede kullacagız:benim calıstırmak ıstedıgım senaryoya isim verebilmemdir.
 
 
 
