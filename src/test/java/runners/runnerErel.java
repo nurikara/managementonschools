08f6a24fa0e768821ef8fd22be89e36b853c71cd
MONSTER <erelozturk2@gmail.com>
2023-06-21 21:30:22 +0300

    v1

--- src/test/java/runners/runnerErel.java
+++ src/test/java/runners/runnerErel.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         monochrome = false,//konsolu okunakli hala getirir false olunca kullanılmayacak.
         features = "src/test/resources/features",
         glue = {"stepdef", "hooks"},//bu parametre ile kodalrımızı yazdıgımız stepDefinition classının package ını belirtiriz.
-        tags = "@deneme",//buraya herhangi birsey yazabiliriz. biz bu tagsı nerede kullacagız:benim calıstırmak ıstedıgım senaryoya isim verebilmemdir.
+        tags = "@erel",//buraya herhangi birsey yazabiliriz. biz bu tagsı nerede kullacagız:benim calıstırmak ıstedıgım senaryoya isim verebilmemdir.
 
 
 
@@ -27,6 +27,6 @@ import org.junit.runner.RunWith;
 )
 
 
-public class runner {
+public class runnerErel {
 
 }
