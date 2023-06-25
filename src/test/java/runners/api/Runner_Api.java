0fff26ebb42768699f93ef6b47392a8aa73516b6
nuri Kara <nurikara@yahoo.com>
2023-06-25 14:12:24 +0300

    sa

--- src/test/java/runners/api/Runner_Api.java
+++ src/test/java/runners/api/Runner_Api.java
@@ -16,11 +16,10 @@ import org.junit.runner.RunWith;
         },
         monochrome = false,//konsolu okunakli hala getirir false olunca kullanılmayacak.
         features = "src/test/resources/features",
-<<<<<<< HEAD
-        glue = {"stepdef", "hooks"},
-=======
+
+
         glue = {"stepdef","hooks"},
->>>>>>> main
+
         tags ="",
         dryRun = false
 )
--- src/test/java/runners/ui/Runner_Ui.java
+++ src/test/java/runners/ui/Runner_Ui.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"stepdef", "hooks"},
 
-        tags ="@US06_Negative",    //       ===>> Burasi bos kalacak
+        tags ="@US06_Positive",    //       ===>> Burasi bos kalacak
 
         dryRun = false
 )
