f9aa42ff475beb4793a4cfb90ad86efefabbf706
MONSTER <erelozturk2@gmail.com>
2023-07-10 21:45:45 +0300

    v43

--- src/test/java/stepdef/api/Api_Erel.java
+++ src/test/java/stepdef/api/Api_Erel.java
@@ -361,5 +361,152 @@ public class Api_Erel extends ManagementSchoolBaseUrl {
         Assert.assertTrue(responseerel.asString().contains("Student Deleted"));
         Assert.assertTrue(responseerel.asString().contains("OK"));
     }
+
+    @Given("ADMİN student icin post request yap {string}, {string}, {string}, {string}, {string}, {string}, {string},{string},{string},{string},{string},{string},{string}")
+    public void adminStudentIcinPostRequestYap(String advisorTeacherId, String birthDay, String birthPlace, String email, String fatherName, String gender, String motherName, String name, String surname, String phoneNumber, String ssn, String username, String password) {
+        Faker fake = new Faker();
+        //set the url
+        //https://managementonschools.com/app/students/save
+        spec.pathParams("first","students","second","save");
+
+        //set the expected data
+        if (advisorTeacherId==null) {
+            advisorTeacherId = null;
+        } else {
+            advisorTeacherId = "1";
+        }
+        if (birthDay.equals("")) {
+            birthDay = "";
+        } else {
+            birthDay = "1979-12-12";
+        }
+        if (birthPlace.equals("")) {
+            birthPlace = "";
+        } else {
+            birthPlace = "Alaska";
+        }
+        if (gender.equals("")) {
+            gender = "";
+        } else {
+            gender = "FEMALE";
+        }
+        if (name.equals("")) {
+            name = "";
+        } else {
+            name = fake.name().firstName();
+        }
+        if (password.equals("")) {
+            password = "";
+        } else {
+            password = (String) ConfigReader.getProperty("passw");
+        }
+        if (phoneNumber.equals("")) {
+            phoneNumber = "";
+        } else {
+            phoneNumber = fake.numerify("###-###-####");
+        }
+        if (ssn.equals("")) {
+            ssn = "";
+        } else {
+            ssn = fake.idNumber().ssnValid();
+        }
+        if (surname.equals("")) {
+            surname = "";
+        } else {
+            surname = fake.name().lastName();
+        }
+        if (username.equals("")) {
+            username = "";
+        } else {
+            username = fake.name().lastName();
+        }
+        if (motherName.equals("")) {
+            motherName = "";
+        } else {
+
+            motherName =(String) ConfigReader.getProperty("motherName");
+        }
+        if (fatherName.equals("")) {
+            fatherName = "";
+        } else {
+
+            fatherName =(String) ConfigReader.getProperty("fatherName");
+        }
+        if (email.equals("")) {
+            email = "";
+        } else {
+
+            email =fake.internet().emailAddress();
+        }
+        if (birthDay.equals("")) {
+            birthDay = "";
+        } else {
+            birthDay = "1979-12-12";
+        }
+
+
+        //  expectedData=new US15_Pojo(1,"1999-04-23","ankara","DDASLM@gmail.com","demir","FEMALE","bade","cemil",
+        //             "cem","379-109-4431","815-69-0011","DADKSdLD","118914697",0,0,null);
+        expectedDataa=new US15_p(advisorTeacherId,birthDay,birthPlace,email,fatherName,gender,motherName,name,surname,phoneNumber,ssn,username,password);
+        //Send the request and get the response
+        responseerel = given(spec).body(expectedDataa).header("Authorization", generateToken((String) ConfigReader.getProperty("ErelusernameAdmin"), (String) ConfigReader.getProperty("ErelpasswordAdmin"))).post("{first}/{second}");
+        responseerel.prettyPrint();
+    }
+
+    @Then("ADMİN body i dogrula")
+    public void adminBodyIDogrula() {
+        US15_ObjectPojo actualData = ObjectMapperUtils.convertJsonToJava(responseerel.asString(),US15_ObjectPojo.class);
+        //  System.out.println("actualData = " + actualData);
+        //   System.out.println("actualData.getObject().getUsername() = " + actualData.getObject().getUsername());
+        // System.out.println("expectedDataa.getUsername() = " + expectedDataa.getUsername());
+        assertEquals(expectedDataa.getUsername(),actualData.getObject().getUsername());
+        assertEquals(expectedDataa.getName(),actualData.getObject().getName());
+        assertEquals(expectedDataa.getSurname(),actualData.getObject().getSurname());
+        assertEquals(expectedDataa.getBirthDay(),actualData.getObject().getBirthDay());
+        assertEquals(expectedDataa.getBirthPlace(),actualData.getObject().getBirthPlace());
+        assertEquals(expectedDataa.getPhoneNumber(),actualData.getObject().getPhoneNumber());
+        assertEquals(expectedDataa.getUsername(),actualData.getObject().getUsername());
+        assertEquals(expectedDataa.getGender(),actualData.getObject().getGender());
+        assertEquals(expectedDataa.getMotherName(),actualData.getObject().getMotherName());
+        assertEquals(expectedDataa.getFatherName(),actualData.getObject().getFatherName());
+        assertEquals(expectedDataa.getEmail(),actualData.getObject().getEmail());
+        jsonPath = responseerel.jsonPath();
+        userId = jsonPath.getInt("object.userId");
+        System.out.println("userId = " + userId);
+    }
+
+    @Given("ADMİN send get request to url by id_")
+    public void adminSendGetRequestToUrlById_() {
+        https://managementonschools.com/app/students/getStudentById?id=632
+        System.out.println("userId = " + userId);
+
+        spec.pathParams("first", "students", "second", "getStudentById").queryParam("id",userId);
+        responseerel = given(spec).header("Authorization", generateToken((String) ConfigReader.getProperty("ErelusernameAdmin"), (String) ConfigReader.getProperty("ErelpasswordAdmin")))
+                .get("{first}/{second}");
+
+        responseerel.prettyPrint();
+    }
+
+    @Then("ADMİN Validate response body_")
+    public void adminValidateResponseBody_() {
+        Assert.assertEquals(200,responseerel.statusCode());
+    }
+
+    @Given("ADMİN send DEL request to url by id_")
+    public void adminSendDELRequestToUrlById_() {
+        //https://managementonschools.com/app/students/delete/504
+        spec.pathParams("first", "students", "second", "delete","third",userId);
+        responseerel = given(spec).header("Authorization", generateToken((String) ConfigReader.getProperty("ErelusernameAdmin"), (String) ConfigReader.getProperty("ErelpasswordAdmin")))
+                .delete("{first}/{second}/{third}");
+
+        responseerel.prettyPrint();
+    }
+
+    @Then("ADMİN Validate DEL response body_")
+    public void adminValidateDELResponseBody_() {
+        Assert.assertEquals(200,responseerel.statusCode());
+        Assert.assertTrue(responseerel.asString().contains("Student Deleted"));
+        Assert.assertTrue(responseerel.asString().contains("OK"));
+    }
 }
 
--- src/test/resources/features/apiFeatures/US25_Erel/US25.CreateStudentByAdmin.feature
+++ src/test/resources/features/apiFeatures/US25_Erel/US25.CreateStudentByAdmin.feature
@@ -1,11 +1,19 @@
-@US15Api
+@US25Api @Api
 Feature:  Aday öğrenciler sisteme kayıt olabilmelidir. API
 
-  Scenario Outline: US25_StudentManagement
+  Scenario Outline: US15_StudentManagement
 
-    Given Admin olarak tum students icin get request yap
-    Then gelen bodyi dogrula "<name>", "<surname>", "<birthplace>", "<phone>", "<gender>", "<Date Of Birth>", "<ssn>","<username>","<fathername>","<mothername>","<password>"
+    Given ADMİN student icin post request yap "<advisorTeacherId>", "<birthDay>", "<birthPlace>", "<email>", "<fatherName>", "<gender>", "<motherName>","<name>","<surname>","<phoneNumber>","<ssn>","<username>","<password>"
+    Then ADMİN body i dogrula
     Examples:
-      | username    | name  | surname | birthplace | phone        | gender | Date Of Birth | ssn         | password | fathername | mothername |
-      | Adminbionic | cemil | cem     | ankara     | 531-432-0021 | FEMALE | 2000-04-23    | 392-81-6745 | 12345678 | demir      | bade       |
+      | advisorTeacherId | birthDay | birthPlace | email | fatherName | gender | motherName | name | surname | phoneNumber | ssn | username | password |
+      | advisorTeacherId | birthDay | birthPlace | email | fatherName | gender | motherName | name | surname | phoneNumber | ssn | username | password |
 
+
+  Scenario: US15_StudentManagement
+    Given ADMİN send get request to url by id_
+    Then ADMİN Validate response body_
+
+  Scenario: US15_StudentManagement
+    Given ADMİN send DEL request to url by id_
+    Then ADMİN Validate DEL response body_
