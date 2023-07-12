233a15ea708e3f08948dc177cd2428df61216942
nuri Kara <nurikara@yahoo.com>
2023-07-13 00:53:58 +0300

    sa

--- src/test/java/runners/api/runnerApi.java
+++ src/test/java/runners/api/runnerApi.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
 
         glue = {"stepdef","hooks"},
-        tags ="@ApiUS_02",
+        tags ="@e2e_Nuri1",
 
         dryRun =false
 
